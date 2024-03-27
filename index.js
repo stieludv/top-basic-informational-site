fa6dab4f72d1fe93ae47a6103f56481cd901d658
stieludv <19951864+stieludv@users.noreply.github.com>
2024-03-27 22:02:47 +0100

    Headings on HTML pages, server redirects to correct page depending on req.url

--- index.js
+++ index.js
@@ -7,14 +7,39 @@ localhost:8080/contact-me should take users to contact-me.html
 
 const http = require('node:http');
 const fs = require('node:fs');
+const path = require('node:path');
 
 const hostname = '127.0.0.1';
 const port = 8080;
 
 const server = http.createServer((req, res) => {
+
+    const uri = req.url === "/" ? "/index" : req.url;
+    const parsedPathname = path.parse(req.url).pathname;
+
+    fs.readFile(`.${uri}.html`, "utf-8", (error, data) => {
+        if (error) {
+            // Errno -2 = file not found
+            if (error.errno = -2) {
+                fs.readFile('./404.html', 'utf-8', (error, data) => {
+                    res.setHeader('Content-Type', 'text/html');
+                    res.statusCode = 404;
+                    res.end(data);
+                });
+            }
+        }
+        else {
+            res.setHeader('Content-Type', 'text/html');
+            res.statusCode = 200;
+            res.end(data);
+        }
+    })
+
+    /*
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello, World!');
+    */
 });
 
 server.listen(port, hostname, () => {
