bfe95fc830a838bcaba049784d7a8c537a7ee2b8
stieludv <19951864+stieludv@users.noreply.github.com>
2024-03-27 21:37:45 +0100

    Returns Hello World on 127.0.0.1:8080 successfully

--- index.js
+++ index.js
@@ -5,3 +5,18 @@ localhost:8080/contact-me should take users to contact-me.html
 404.html should display any time the user tries to go to a page not listed above.
 */
 
+const http = require('node:http');
+const fs = require('node:fs');
+
+const hostname = '127.0.0.1';
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, World!');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});     
